035ab5b17fcf18d2d6619ce66b1b70a10fbc1f7a
Mariana Torres <tcatalina93@gmail.com>
2023-01-28 18:44:22 -0500

    Saludo, agregado

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -3,10 +3,17 @@ package javaapplication1;
 
 // @author Mariana
 
+import java.util.Scanner;
+
+
 public class JavaApplication1 {
 
     public static void main(String[] args) {
+        Scanner leer = new Scanner (System.in);
         System.out.println("Holaaaaa");
+        System.out.println("Como estás?");
+        String resp = leer.nextLine();
+        System.out.println(resp);
     }
 
 }
